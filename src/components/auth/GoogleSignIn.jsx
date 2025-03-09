486f0e93d68a3dbf74f13f5d375b9b832cd2a5b1
Amy T <amycaitjea@gmail.com>
2025-03-09 13:05:42 +1100

    Improved button css

--- src/components/auth/GoogleSignIn.jsx
+++ src/components/auth/GoogleSignIn.jsx
@@ -1,16 +1,30 @@
-import { signIn } from "@/auth"
-import styles from './GoogleSignIn.module.css'
- 
+import { signIn } from "@/auth";
+import "./GoogleSignIn.css";
+import GoogleIcon from "./google_icon.svg";
+import Image from "next/image";
+
 export default function SignIn() {
   return (
     <form
       action={async () => {
-        "use server"
-        await signIn("google",{redirectTo:"/home"})
+        "use server";
+        await signIn("google", { redirectTo: "/home" });
       }}
     >
-      <button className={styles.customBtn}
-      type="submit">Sign in with Google</button>
+      <button className="gsi-material-button">
+        <div className="gsi-material-button-state"></div>
+        <div className="gsi-material-button-content-wrapper">
+          <Image
+            className="gsi-material-button-icon"
+            alt="Google Icon"
+            src={GoogleIcon}
+          />
+          <span className="gsi-material-button-contents">
+            Sign in with Google
+          </span>
+          <span style={{ display: "none" }}>Sign in with Google</span>
+        </div>
+      </button>
     </form>
-  )
-} 
+  );
+}
