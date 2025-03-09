2e69ec2e3d4e176ba6a0f5c852726900243112c1
Amy T <amycaitjea@gmail.com>
2025-03-09 13:06:08 +1100

    Ensure background image covers entire background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import GoogleSignIn from "@/components/auth/GoogleSignIn"
 
 export default function Login() {
   return (
-    <div >
-     <h1>Get it Together</h1> 
-     <p>Task Management System for Students</p> 
-
-      <GoogleSignIn />
-
-
-
+    <><div>
+      <h1>Get it Together</h1>
+      <p>Task Management System for Students</p>
     </div>
+      <GoogleSignIn />
+    
+    </>
+
+
+
+
   );
 }
