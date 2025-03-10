9b32463080cc05e241e285f12c28bcc5a1ca44ce
Mitchell Whitten <mitchyw87@gmail.com>
2025-03-11 08:22:55 +1100

    Cleaned up structure of file to prepare for main merge

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
-import type { NextConfig } from "next";
+const webpack = require('webpack');
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^react-native-sqlite-storage$/,
+      }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^@sap\/hana-client\/extension\/Stream$/,
+      }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^mysql$/,
+      })
+    );
+
+    // Optionally ignore specific warnings from TypeORM
+    config.ignoreWarnings = [
+      {
+        // Ignore warnings in ConnectionOptionsReader
+        module: /typeorm\/connection\/ConnectionOptionsReader\.js/,
+      },
+      {
+        // Ignore warnings in DirectoryExportedClassesLoader
+        module: /typeorm\/util\/DirectoryExportedClassesLoader\.js/,
+      },
+    ];
+
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
