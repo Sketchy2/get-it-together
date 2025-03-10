beb930d54a8b2d4d5baa91a39b63f86a19b74d06
Mitchell Whitten <mitchyw87@gmail.com>
2025-03-11 08:23:00 +1100

    Cleaned up structure of file to prepare for main merge

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Landing() {
+    return (
+      <>
+        <div className="">
+        <h1>Loading  Application ...</h1>
+        </div>
+  
+      </>
+  
+  
+  
+  
+    );
+  }
