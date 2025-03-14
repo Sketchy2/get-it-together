d304cebbb57a3b32016093617f8df3ec512ebb98
Amy Tjea <amycaitjea@gmail.com>
2025-03-14 11:59:56 +1100

    Restored css

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,27 @@
-import GoogleSignIn from "@/components/GoogleSignIn"
-import EmailSignIn from "@/components/EmailSignIn"
+import GoogleSignIn from "@/components/auth/GoogleSignIn"
+import "./login.css"
+
 export default function Login() {
   return (
-    <div >
-     <h1>Get it Together</h1> 
-     <p>Task Management System for Students</p> 
-
+    <>
+      <span></span>
+      <main className="title">
+        <div>
+        <h1>Get it Together</h1>
+        <p>Task Management System for Students</p>
+        
       <GoogleSignIn />
-      <p>or</p> 
-      <EmailSignIn />
+      <p>or</p>
+      <p>Sign in with Email</p>
 
 
-    </div>
+        </div>
+      </main>
+
+    </>
+
+
+
+
   );
 }
