cbf93b98e05a3e7902e7556de2a04cb8f6e40f2e
Amy Tjea <amycaitjea@gmail.com>
2025-04-15 12:42:25 +1000

    Init attempt with pie char

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,65 @@
-import React from 'react'
-import Link from 'next/link'
+"use client"
+import React, { useState } from 'react'
 import "./NavBar.css"
 
+export default function NavBar(){
+    const menuItems = ["Assign", "Tsks", "Schedule", "Settings"]
 
-export default function NavBar() {
-  return (
-    <nav >NavBar
-    <ul>    
-    <li><Link href="/task">Tasks</Link></li>
-    <li><Link href="/assignment">Assignments</Link></li>
-    <li><Link href="/schedule">Scheduling</Link></li>
-    <li><Link href="/setting">Settings</Link></li>
-    </ul>
-
-    </nav>
+  return(
+    <>
+<div className="palette">
+  <div className="color1"></div>
+  <div className="color2"></div>
+  <div className="color3"></div>
+  <div className="color4"></div>
+  <div className="color5"></div>
+</div>
+</>
   )
 }
+
+// export default function NavBar() {
+//   const menuItems = ["Assign", "Tsks", "Schedule", "Settings"]
+//   const colors = ["red", "blue", "green", "yellow"]
+//   const [isOpen, setIsOpen] = useState(false);
+//   const centralAngle = 90 / menuItems.length;
+
+//   const transformItem = (idx: number): string => {
+//     return `rotate(${centralAngle * idx + 190}deg) skew(${90 - centralAngle}deg)`
+//   }
+
+//   const transformInnerContent = (idx: number): string => {
+//     return `rotate(${-(90 - (centralAngle * idx / 2) + 90)}deg) skew(${-(90 - centralAngle)}deg)`
+//   }
+
+
+//   return (
+
+//     <>
+//       <button onClick={() => setIsOpen(!isOpen)}>Menu</button>
+//       <ul style={{ width: "300px", height: "150px", backgroundColor: "grey" }}>
+//         {
+//           menuItems.map((item, idx) =>
+//             <li
+//               key={idx}
+//               style={{
+//                 "display": isOpen ? "block" : "none",
+//                 "transform": transformItem(idx),
+//               }}
+//             >
+//               <a style={{
+//                 "transform": transformInnerContent(idx)
+//                 ,"backgroundColor": colors[idx]
+//                 ,position: "absolute"
+//                 , textAlign: "center"
+//               }}>{item}</a>
+//             </li>
+//           )
+//         }
+
+//       </ul>
+//     </>
+
+//   )
+
+// }
