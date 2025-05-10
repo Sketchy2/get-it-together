d30213ebdb0cb8e4a8d783858566002a3eae529a
Amy Tjea <amycaitjea@gmail.com>
2025-05-10 14:55:31 +1000

    Improved appearance of no assignments on the assignment page

--- src/app/(main_app)/assignment/page.tsx
+++ src/app/(main_app)/assignment/page.tsx
@@ -358,17 +358,23 @@ export default function Assignments() {
     )
   }
 
-  if (assignments.length == 0) {
+  if (assignments.length== 0) {
     //TODO: STYLE THIS
     return (
-      <div className="loadingContainer">
-        <p>No assignments...</p>
-        <p>Time to get working!</p>
+      <><div className="loadingContainer">
+        <h2>No assignments...</h2>
+        <h3>Time to get working!</h3>
         <div className="addCard" onClick={() => setIsCreateModalOpen(true)}>
           <PlusIcon size={24} />
           <span>Add Assignment</span>
         </div>
       </div>
+      
+      <CreateAssignmentModal
+          isOpen={isCreateModalOpen}
+          onClose={() => setIsCreateModalOpen(false)}
+          onSave={handleCreateAssignment}
+          assignment={null} /></>
     )
   }
 
