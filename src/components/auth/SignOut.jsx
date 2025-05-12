5e9b8ab0d627a9eff77316ecf7bd65f17238ae99
Mitchell Whitten <mitchyw87@gmail.com>
2025-05-12 15:04:58 +1000

    fixed signout

--- src/components/auth/SignOut.jsx
+++ src/components/auth/SignOut.jsx
@@ -1,17 +1,25 @@
-import { SignOutAction } from "@/components/auth/SignOutAction"
-import { LogOut } from "lucide-react";
-import Router from "next/router";
+"use client"
+
+import { SignOutAction } from "./SignOutAction"
+import { useRouter } from "next/navigation"
+import { LogOut } from "lucide-react"
 import "./SignOut.css"
- 
+
 export default function SignOut() {
+  const router = useRouter()
+
+  const handleSignOut = async (e) => {
+    e.preventDefault()
+    await SignOutAction()
+    router.push("/")
+  }
+
   return (
-    <form
-      action={async () => {
-        Router.push("./");
-              await SignOutAction();
-          }}
+    <button 
+      className="signOutButton" 
+      onClick={handleSignOut}
     >
-      <button className="signOutButton" type="submit"><LogOut size={16} />Sign Out</button>
-    </form>
+      <LogOut size={16} /> Sign Out
+    </button>
   )
-} 
+}
