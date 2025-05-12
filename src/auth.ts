2a9015f1ed2b32f0243cea05b5ff0027ba34cb30
Mitchell Whitten <mitchyw87@gmail.com>
2025-05-12 15:11:34 +1000

    .

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import { TypeORMAdapter } from "@auth/typeorm-adapter";
 import authConfig from "./auth.config";
-import { typeormOptions } from "./typeorm-datasource"; // <-- importing raw options not datasource
+import { typeormOptions } from "./typeorm-datasource";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: TypeORMAdapter(typeormOptions),
